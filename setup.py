ec4795ef9ce50042958724ea42bc1239791fe0d7
Calder Coalson <caldercoalson@gmail.com>
2014-09-27 13:58:33 -0700

    Hacks to install script in /usr/local/bin on Mac OS X.

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
-from setuptools import setup
+from setuptools import *
 
 setup(
     name="babel",
-    version="0.1.0",
+    version="0.3.0",
     author="Calder Coalson",
     author_email="caldercoalson@gmail.com",
-    packages=["babel"],
     url="https://github.com/Calder/Babel-Bin",
+    description = "Foo bar",
+    long_description=open("README.txt").read(),
     license="LICENSE.txt",
-    description = "Useful towel-related stuff.",
-    long_description=open("README.md").read(),
+    
     install_requires=[
         "requests-futures",
         "twilio",
     ],
+    zip_safe=True,
+
+    packages=["babel"],
     entry_points={
         "console_scripts": [
             "babel=babel:main",
         ],
     },
 )
+
+import sys
+if sys.prefix != "/usr/local/bin":
+    import subprocess
+    subprocess.call(["ln", "-sf",
+                     "%s/bin/babel" % sys.prefix, 
+                     "/usr/local/bin/babel"])
