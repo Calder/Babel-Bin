4a27f34edeb518fd5f7fb6825d789775466a109d
Calder Coalson <caldercoalson@gmail.com>
2014-09-27 14:01:21 -0700

    Cleanup.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import *
 
 setup(
+    # Metadata
     name="babel",
     version="0.3.0",
     author="Calder Coalson",
@@ -9,24 +10,29 @@ setup(
     description = "Foo bar",
     long_description=open("README.txt").read(),
     license="LICENSE.txt",
-    
-    install_requires=[
-        "requests-futures",
-        "twilio",
-    ],
-    zip_safe=True,
 
+    # Contents
     packages=["babel"],
     entry_points={
         "console_scripts": [
             "babel=babel:main",
         ],
     },
+
+    # Dependencies
+    install_requires=[
+        "requests-futures",
+        "twilio",
+    ],
+
+    # Settings
+    zip_safe=True,
 )
 
+# Hack to install script in /usr/local/bin on Mac OS X
 import sys
 if sys.prefix != "/usr/local/bin":
     import subprocess
     subprocess.call(["ln", "-sf",
-                     "%s/bin/babel" % sys.prefix, 
+                     "%s/bin/babel" % sys.prefix,
                      "/usr/local/bin/babel"])
